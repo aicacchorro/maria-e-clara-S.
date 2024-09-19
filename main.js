62d5dacd93a06a026165b114eb7229300df43374
aicacchorro <166017744+aicacchorro@users.noreply.github.com>
2024-09-19 12:07:06 +0000

    pagina js

---
+++ main.js
@@ -0,0 +1,7 @@
+const botoes = document.querySelectorAll(".botao");
+const botoes = documente.querySelectorAll("botao");
+
+for(let j = 0; j <botoes.length; j++){
+    botoes[j].onclick = function(){
+    ]
+}
